5075a253b9fdaa516b1266deb658ab9686110875
Maor Israel <maorisr@gmail.com>
2021-02-28 20:37:26 +0200

    temp commit

--- index.ts
+++ index.ts
@@ -23,5 +23,5 @@ const host = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 app.listen(+port, host, function() {
-    console.log("Server started.......");
+    console.log("Server started......");
 });
