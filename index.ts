42fbde108cf40961e4438926ae1c76986acddd09
Maor Israel <maorisr@gmail.com>
2021-02-28 20:30:39 +0200

    temp commit

--- index.ts
+++ index.ts
@@ -7,19 +7,20 @@ const app = express();
 const cors = require('cors')
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 PersistRepository.init();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!' +
-      '')
+    res.send('Hello World!' +
+        '')
 })
 
 app.use('/guests', guests);
 
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000...')
-})
+app.listen(process.env.PORT || 5000
+    , function () {
+        console.log('Listening on port 3000...')
+    })
