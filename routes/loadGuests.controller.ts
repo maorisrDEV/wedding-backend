e64c0c4ed33bb1b8449c8b647214cc4700f847bd
Maor Israel <maorisr@gmail.com>
2021-05-29 19:44:58 +0300

    added apis for status proj

--- routes/loadGuests.controller.ts
+++ routes/loadGuests.controller.ts
@@ -5,6 +5,10 @@ export class loadGuestsController {
 
     public static async loadGuests(req, res) {
         const guests = req?.body;
+        // casting amountOfInvited from string to number
+        guests.forEach(guest => {
+            guest.amountOfInvited = +guest.amountOfInvited;
+        })
         const response = await MongodbRepository.loadGuests(guests);
         res.json(response);
     }
